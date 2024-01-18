5839eacff327d36c71ac0bbfcbc3a5fe84109002
suryathakur15 <152003335+suryathakur15@users.noreply.github.com>
2024-01-18 16:24:29 +0000

    Deploying to public from @ suryathakur15/portfolio@11e97ed3071605c15e1062d55b54a28d39b5f709 🚀

---
+++ _next/static/oTaqi7DyzEjvv7zl6zuLa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/419-e71efdbc79bf994f.js","static/css/478962006c1993be.css","static/chunks/pages/index-62ce7d6debfe9c31.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
