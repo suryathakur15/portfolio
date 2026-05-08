13fa206234f45d2f25bf99bfcdcf85b4f861abc5
Surya Thakur <suryapratap1515@gmail.com>
2026-05-08 17:09:51 +0530

    fix: use unoptimized images for static export

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,13 @@
  */
 const nextConfig = {
   images: {
-    loader: "akamai",
-    path: "/",
+    unoptimized: true,
   },
   trailingSlash: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
 };
